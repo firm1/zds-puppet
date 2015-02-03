048898d112ad9e6c0fae1808e1b084615b770d33
firm1 <dralliw350@gmail.com>
2015-02-03 15:07:09 +0000

    init.pp edited online with Bitbucket

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,13 +29,65 @@
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Author Name <firm1@zestedesavoir.com>
 #
 # === Copyright
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class zds {
+class zds (
+	$url = $zds::params::url,
+	$repo = $zds::params::repo,
+	$branch = $zds::params::branch,
+	$id = $zds::params::id
+) inherits ::zds::params {
+	include nginx
+    include python
+    include mysql
+    include nodejs
+  
+    package {
+      "git-core": ensure => latest;
+      "texlive": ensure => latest;
+      "texlive-xetex": ensure => latest;
+      "texlive-lang-french": ensure => latest;
+      "texlive-latex-extra": ensure => latest;
+    }
+    package { 'npm':
+      ensure   => latest,
+      provider => 'npm',
+    }
 
+    exec {"git-clone":
+      command => "git clone -b ${branch} --single-branch git://github.com/${repo}/zds-site.git /opt/${id}/zds-site",
+      creates => /opt/${id}/zds-site,
+      require => [Package['git-core'], File['/opt/${id}']]
+    }
 
+	python::virtualenv { '/opt/${id}/venv' :
+	  ensure       => present,
+	  version      => 'system',
+	  requirements => '/opt/${id}/zds-site/requirements.txt',
+	  systempkgs   => true,
+	  distribute   => false,
+	}
+
+	python::gunicorn { 'vhost-${id}' :
+	  ensure      => present,
+	  virtualenv  => '/opt/${id}/venv',
+	  mode        => 'wsgi',
+	  dir         => '/opt/${id}/zds-site/',
+	  bind        => 'unix:/tmp/gunicorn.socket',
+	  environment => 'prod',
+	  appmodule   => 'zds',
+	  osenv       => { 'DBHOST' => 'localhost' },
+	  timeout     => 30,
+	  template    => 'python/gunicorn.erb',
+	}
+
+    nginx::resource::vhost { '${url}/${id}':
+      www_root => '/opt/${id}/zds-site',
+      require => Exec['git-clone']
+    }
+	
 }
