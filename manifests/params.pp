956a76fefd6ebdd788d2d5e2c134e90171f1a5c3
firm1 <dralliw350@gmail.com>
2015-02-03 14:50:37 +0000

    params.pp created online with Bitbucket

---
+++ manifests/params.pp
@@ -0,0 +1,9 @@
+class zds::params {
+    $zds_params = hiera_hash('zds')
+    validate_hash($zds_params)
+    
+    $url = $zds_params['url']
+	$repo = $zds_params['repo'],
+	$branch = $zds_params['branch'],
+	$id = $zds_params['id'],
+}
