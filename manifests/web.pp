1b54ba5192f2f2cdc3cee95294a7ff5bad16381d
firm1 <dralliw350@gmail.com>
2015-02-19 14:37:57 +0000

    web.pp created online with Bitbucket

---
+++ manifests/web.pp
@@ -0,0 +1,29 @@
+class zds::web(
+    $venv_path = $zds::venv_path,
+    $webapp_path = $zds::webapp_path,
+    $id = $zds::id,
+) {
+
+    include nginx
+
+    nginx::resource::upstream { "puppet_zds_app_${id}":
+      members => [
+        "unix:/tmp/gunicorn-${id}.sock"
+      ]
+    }
+
+    nginx::resource::vhost {"vhost-${id}":
+      proxy => "http://puppet_zds_app_${id}",
+      server_name => ["${url}"],
+      access_log => "${venv_path}/logs/nginx_access.log",
+      error_log => "${venv_path}/logs/nginx_error.log",
+      require => File["${venv_path}/logs"]
+    }
+
+    nginx::resource::location {"${id}_static":
+      ensure => present,
+      vhost => "vhost-${id}",
+      location => "/static/",
+      www_root => "${webapp_path}"
+    }
+}
