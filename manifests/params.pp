2aea839df4c463e833dc2f70e1855b74e158a616
firm1 <dralliw350@gmail.com>
2015-02-19 11:24:44 +0000

    params.pp edited online with Bitbucket

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,23 +2,22 @@ class zds::params {
     $zds_params = hiera_hash('zds')
     validate_hash($zds_params)
 
-    $url = $zds_params['url']
-    #$repo = $zds_params['repo']
-    #$branch = $zds_params['branch']
-    #$id = $zds_params['id']
+    $url = $zds_params['site']['url']
+    $repo = $zds_params['repo']['author']
+    $branch = $zds_params['repo']['branch']
+    $id = $zds_params['site']['id']
 
-    #$url = "vps137741.ovh.net"
-    $repo = "zestedesavoir"
-    $branch = "dev"
-    $id = "daily"
-    $database_name = "zdsbase"
-    $database_host = "localhost"
-    $database_user = "root"
-    $database_password = "SuperPassword"
+    $database_name = ['database']['name']
+    $database_host = ['database']['host']
+    $database_user = ['database']['user']
+    $database_password = ['database']['password']
+
     $venv_path = "/opt/${id}/venv"
     $webapp_path = "/opt/${id}/zds-site"
-    $node_version = "v0.10.36"
+    $node_version = $zds_params['front']['node_version']
     $pandoc_repo = "jgm"
     $pandoc_release_tag = "1.13"
     $pandoc_dest = ""
+    
+    $zds_settings = $zds_params['settings']
 }
