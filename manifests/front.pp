9f6df3f4ed398dc36ccf50a4cda34d030c519911
firm1 <dralliw350@gmail.com>
2015-02-19 15:00:49 +0000

    front.pp edited online with Bitbucket

--- manifests/front.pp
+++ manifests/front.pp
@@ -13,32 +13,32 @@ class zds::front(
        path  => "${webapp_path}/assets/scss/variables/_colors.scss",
        line  => "\$color-primary: ${primary_color};",
        match => '^\$color-primary*',
-    } ~>
+    } ->
     file_line { 'secondary_color':
        path  => "${webapp_path}/assets/scss/variables/_colors.scss",
        line  => "\$color-secondary: ${secondary_color};",
        match => '^\$color-secondary*',
-    } ~>
+    } ->
     file_line { 'side_bg':
        path  => "${webapp_path}/assets/scss/variables/_colors.scss",
        line  => "\$color-sidebar-background: ${side_bg};",
        match => '^\$color-sidebar-background*',
-    } ~>
+    } ->
     file_line { 'side_hv':
        path  => "${webapp_path}/assets/scss/variables/_colors.scss",
        line  => "\$color-sidebar-hover: ${side_hv};",
        match => '^\$color-sidebar-hover*',
-    } ~>
+    } ->
     file_line { 'body_bg':
        path  => "${webapp_path}/assets/scss/variables/_colors.scss",
        line  => "\$color-body-background: ${body_bg};",
        match => '^\$color-body-background*',
-    } ~>
+    } ->
     file_line { 'header_hv':
        path  => "${webapp_path}/assets/scss/variables/_colors.scss",
        line  => "\$color-header-hover: ${header_hv};",
        match => '^\$color-header-hover*',
-    } ~>
+    } ->
     exec {"update-npm":
        command => "npm install -g npm",
        path => "/usr/local/node/node-default/bin",
@@ -51,22 +51,22 @@ class zds::front(
         environment => ["HOME=/root"],
         require => Exec["update-npm"],
         timeout => 0
-    } ~>
+    } ->
     exec {"front-clean":
         command => "npm run gulp -- clean",
         cwd => "${webapp_path}",
         path => ["/usr/local/node/node-default/bin","/usr/local/bin","/bin"],
         require => Exec["front-prod"]
-    } ~>
+    } ->
     exec {"front-build":
         command => "npm run gulp -- build",
         cwd => "${webapp_path}",
         path => ["/usr/local/node/node-default/bin","/usr/local/bin", "/bin"],
         require => Exec["front-clean"]
-    } ~>
+    } ->
     file { "${webapp_path}/static":
         ensure => directory,
-    } ~>
+    } ->
     exec { "collectstatic":
         command => "${venv_path}/bin/python ${webapp_path}/manage.py collectstatic --noinput --clear",
         require => [Exec['front-build'], File["${webapp_path}/static"]]
